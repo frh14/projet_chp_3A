3f7c6787d2facc3782b2b25cee920cf9e3c7e065
FR <frh01499@gmail.com>
2021-10-27 17:12:38 +0200

    clean 2

--- fonctions.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _FONCTIONS_HPP_
-#define _FONCTIONS_HPP_
-
-double f (double x, double y, double t, double Lx, double Ly, int mode);
-double g (double x, double y, int mode);
-double h (double x, double y, int mode);
-
-#endif // _FONCTIONS_HPP_
--- matrix.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _MATRIX_HPP_
-#define _MATRIX_HPP_
-
-void sparseMatrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &value, int Nx, int Ny, double Lx, double Ly, double D, double dt);
-void secondMembre(std::vector<double> &S,std::vector<double> U, int Nx, int Ny, double dt,double t, double Lx, double Ly, double D,int mode);
-void sparseMatrix_parallel(std::vector<int> &row,std::vector<int> &col,std::vector<double> &value, int Nx, int Ny, double Lx, double Ly, double D, double dt,int iBeg,int iEnd);
-void secondMembre_parallel(std::vector<double> &S,std::vector<double> U, int Nx, int Ny,double dt, double t, double Lx, double Ly, double D, int mode,int iBeg,int iEnd);
-
-#endif // _MATRIX_HPP_
