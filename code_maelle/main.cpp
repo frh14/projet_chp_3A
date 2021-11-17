3e53df1c6e201150f1ff858af9c06fddcac2916b
alixkien <alix.kien@gmail.com>
2021-11-17 17:11:21 +0100

    boucle schwarz

--- code_maelle/main.cpp
+++ code_maelle/main.cpp
@@ -15,117 +15,149 @@
 
 using namespace std;
 
-//résolution de l'équation de diffusion sur un domaine Omega partitionné avec en deux sous domaine Omega1 et Omega2
-//conditions aux limites sur les bords Gamma1 et Gamma2 de type non homogéne données par des fonctions
-//le domaine est scindé en deux selon ses lignes
+//résolution de l'equation de diffusion sur un domaine Omega partitionne avec en deux sous domaine Omega1 et Omega2
+//conditions aux limites sur les bords Gamma1 et Gamma2 de type non homogene données par des fonctions
+//le domaine est scinde en deux selon ses lignes
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv){
 
-  // Lecture des données dans le fichier paramètres (parameters.txt)
+  // Lecture des donnees dans le fichier parametres (parameters.txt)
   ifstream file("parameters.txt");
   int Nx(0), Ny(0);        // Nombre de noeuds en x et en y
-  double Lx(0), Ly(0); //Taille du rectangle
+  double Lx(0), Ly(0); // Taille du rectangle
   double D(0), dt(0); // Coefficient de diffusion et pas de temps
   int mode(0);  // Variable qui permet de choisir quelles fonctions f,g,h sont prises
-  int h_part(0); // paramétre de recouvrement: nombre de lignes partagées par chaque sous-domaine
+  int h_part(0); // parametre de recouvrement: nombre de lignes partagees par chaque sous-domaine
   double alpha(0), beta(0); // Coefficients de la condition d'interface (Neumann-Dirichlet)
   file >> Nx >> Ny >> Lx >> Ly >> D >> dt >> mode >> h_part >> alpha >> beta ;
   file.close();
 
   //------------------------------------------------------------------------------------------------------------------------------------
-//résolution séquentielle du problème de diffusion
-//----------------------------------------------------------------------------------------------------------------------------------------
+  //resolution sequentielle du probleme de diffusion
+  //----------------------------------------------------------------------------------------------------------------------------------------
 
   struct timeval t1,t2;
   gettimeofday(&t1, NULL);
 
-  //construction de nos indices de séparation de domaine selon ses lignes
+  //construction de nos indices de separation de domaine selon ses lignes
   int Nu(0); //domaine 1
   int Nv(0); //domaine 2
-  //test sur la parité de Nx
+  //test sur la parite de Nx
   if (Nx%2==0) {
     Nu=Nx/2;
-    Nv=Nx/2+h_part;}
+    Nv=Nx/2+h_part;
+  }
   else if (Nx%2==1) {
     Nu=(Nx-1)/2;
-    Nv=(Nx-1)/2+h_part;}
+    Nv=(Nx-1)/2+h_part;
+  }
 
   //construction des vecteurs inconnues sur chaque sous domaine au temps initial:
   std::vector<double> U(Nu*Ny,1); //domaine 1
   std::vector<double> V(Nv*Ny,1); //domaine 2
 
-  //construction des matrices de résolution
+  //construction des stencils
+  std::vector<double> U0(3*Ny); //domaine 1 pour le domaine 2
+  std::vector<double> V0(3*Ny); //domaine 2 pour le domaine 1
+
+  //construction des matrices de resolution
   std::vector<int> rowu, rowv;
   std::vector<int> colu, colv;
   std::vector<double> valu, valv;
   //domaine 1
-  int me=0;
-  Matrix(rowu,colu,valu,Nx,Ny,Nu,Nv,Lx,Ly,D,dt,alpha,beta,me);
+  Matrix(rowu,colu,valu,Nx,Ny,Nu,Nv,Lx,Ly,D,dt,alpha,beta,0);
   //domaine 2
-  me=1;
-  Matrix(rowv,colv,valv,Nx,Ny,Nu,Nv,Lx,Ly,D,dt,alpha,beta,me);
+  Matrix(rowv,colv,valv,Nx,Ny,Nu,Nv,Lx,Ly,D,dt,alpha,beta,1);
 
   /*//affichage pour phase de test
-printf("Nx=%d et Ny=%d \n",Nx,Ny);
-printf("Nu=%d et Nv=%d \n",Nu,Nv);
-double dx=Lx/(Nx+1),dy=Ly/(Ny+1);
-double di(0),sdi(0),ssdi(0);
-di=1+2*dt*(1/pow(dx,2)+1/pow(dy,2));
-sdi=-dt*D/pow(dx,2);
-ssdi=-dt*D/pow(dy,2);
-printf("------------------------------------------------------------------------\n");
-printf("di=%f \n",di);
-printf("sdi=%f \n",sdi);
-printf("ssdi=%f \n",ssdi);
-printf("di+D*dt*beta/(dx*alpha))=%f \n",di+D*dt*beta/(dx*alpha));
-printf("2*sdi=%f \n",2*sdi);
+  printf("Nx=%d et Ny=%d \n",Nx,Ny);
+  printf("Nu=%d et Nv=%d \n",Nu,Nv);
+  double dx=Lx/(Nx+1),dy=Ly/(Ny+1);
+  double di(0),sdi(0),ssdi(0);
+  di=1+2*dt*(1/pow(dx,2)+1/pow(dy,2));
+  sdi=-dt*D/pow(dx,2);
+  ssdi=-dt*D/pow(dy,2);
+  printf("------------------------------------------------------------------------\n");
+  printf("di=%f \n",di);
+  printf("sdi=%f \n",sdi);
+  printf("ssdi=%f \n",ssdi);
+  printf("di+D*dt*beta/(dx*alpha))=%f \n",di+D*dt*beta/(dx*alpha));
+  printf("2*sdi=%f \n",2*sdi);
 
-printf("------------------------------------------------------------------------\n");
-printf("------------------------------------------------------------------------\n");
-printf("matrice domaine 1 de taille %d \n",Nu*Ny);
-for (int i = 0; i < Nu*Ny; i++) {
+  printf("------------------------------------------------------------------------\n");
+  printf("------------------------------------------------------------------------\n");
+  printf("matrice domaine 1 de taille %d \n",Nu*Ny);
+  for (int i = 0; i < Nu*Ny; i++) {
   for (int k = 0; k < rowu.size(); k++) {
-    if (rowu[k]==i) {
-      printf(" ligne=%d , colonne=%d , valeur=%f \n",rowu[k],colu[k],valu[k]);}}}
+  if (rowu[k]==i) {
+  printf(" ligne=%d , colonne=%d , valeur=%f \n",rowu[k],colu[k],valu[k]);}}}
 
-printf("------------------------------------------------------------------------\n");
-printf("matrice domaine 2 de taille %d \n",Nv*Ny);
-for (int i = 0; i < Nv*Ny; i++) {
+  printf("------------------------------------------------------------------------\n");
+  printf("matrice domaine 2 de taille %d \n",Nv*Ny);
+  for (int i = 0; i < Nv*Ny; i++) {
   for (int k = 0; k < rowv.size(); k++) {
-      if (rowv[k]==i) {
-          printf(" ligne=%d , colonne=%d , valeur=%f \n",rowv[k],colv[k],valv[k]);}}}*/
-
-  int Nt=100; // nombre d'itérations en temps
-  double e=0.00000001; //tolerance pour le GC
-  int kmax=Nx*Ny; //itération max du GC
+  if (rowv[k]==i) {
+  printf(" ligne=%d , colonne=%d , valeur=%f \n",rowv[k],colv[k],valv[k]);}}}*/
 
   std::string prefixe = "solution_approchée_seq_t=";
 
-  //création des abcisses et ordonnées des points du maillage
-  std::vector<double> x_tab(Nx),y_tab(Ny);
+  //creation des abcisses et ordonnees des points du maillage
+  std::vector<double> x_tab(Nx), y_tab(Ny);
   for (int i = 0; i < Nx; i++) x_tab[i]=(i+1)*Lx/(Nx+1);
   for (int j = 0; j < Ny; j++) y_tab[j]=(j+1)*Ly/(Ny+1);
+
+  int Nt=100; // nombre d'iterations en temps
+  //double e=0.00000001; //tolerance pour le GC
+  //int kmax=Nx*Ny; //iteration max du GC
+  // Parametres d'arret pour Schwarz
+  double errschwz = 0.00000001;
+  int maxschwz = Nx*Ny;
+  double error; int iteschwz;
 
   //schema en temps
   for (int k = 0; k < Nt; k++) {
 
-    double t=k*dt; //temps de l'expérience
+    double t = k*dt; //temps de l'experience
 
-    //construction du second membre sur chaque sous-domaines
-    std::vector<double> S(Nx*Ny,0);
-    //secondMembre(S,U,Nx,Ny,dt,t,Lx,Ly,D,mode);
+    //Initialisation des stencils
+    U0=1; V0=1;
 
-    //résolution du systéme linéaire sur chaque sous-domaine
+    //Boucle de Schwarz
+    while ((iteschwz <= maxschwz)||(error >= errschwz)){
+      //construction du second membre sur chaque sous-domaine
+      std::vector<double> S1(Nx*Ny,0);
+      std::vector<double> S2(Nx*Ny,0);
+      secondMembre(S1,U,V0,Nx,Ny,Nu,dt,t,Lx,Ly,D,mode,alpha,beta,0);
+      secondMembre(S2,V,U0,Nx,Ny,Nv,dt,t,Lx,Ly,D,mode,alpha,beta,1);
 
-//écriture dans les  fichiers
-/*
- std::ostringstream oss;
- oss<<prefixe<<k<<".txt";
- std::ofstream NewFichier(oss.str().c_str(), std::ios::out | std::ios::trunc);
+      //résolution du systéme linéaire sur chaque sous-domaine
+      U = BCGS(S1,0);
+      V = BCGS(S2,0);
 
- for (int i = 0; i < Nx; i++) {
-   for (int j = 0; j < Ny; j++) {
-     NewFichier<< x_tab[i] <<" "<<  y_tab[j] << " " << U[i+j*Nx] << endl;}}*/
+      //mise a jour des stencils
+      for (int j = 0; j < Ny; j++){
+        U0[j] = V[Ny*(h_part-1)+j];
+        U0[Ny+j] = V[Ny*(h_part)+j];
+        U0[2*Ny+j] = V[Ny*(h_part+1)+j];
+      }
+      for (int j = 0; j < Ny; j++){
+        V0[j] = U[Ny*(Nu-h_part-1)+j];
+        V0[Ny+j] = U[Ny*(Nu-h_part)+j];
+        V0[2*Ny+j] = U[Ny*(Nu-h_part+1)+j];
+      }
+      // Mettre une evaluation de l'erreur pour la condition d'arret error
+      iteschwz++;
+    }
+
+    //ecriture dans les  fichiers
+    /*
+    std::ostringstream oss;
+    oss<<prefixe<<k<<".txt";
+    std::ofstream NewFichier(oss.str().c_str(), std::ios::out | std::ios::trunc);
+
+    for (int i = 0; i < Nx; i++) {
+    for (int j = 0; j < Ny; j++) {
+    NewFichier<< x_tab[i] <<" "<<  y_tab[j] << " " << U[i+j*Nx] << endl;}}*/
 
   }
   gettimeofday(&t2, NULL);
@@ -135,7 +167,7 @@ for (int i = 0; i < Nv*Ny; i++) {
   printf("temps écoulé =%lu \n",t2.tv_usec - t1.tv_usec);
 
   //-----------------------------------------------------------------------------------------------------------------------------------------
-  //fin de la résolution séquentielle
+  //fin de la resolution sequentielle
   //------------------------------------
 
   return 0;
--- code_maelle/matrix.cpp
+++ code_maelle/matrix.cpp
@@ -18,20 +18,20 @@ void Matrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val
   di=1-2*dt*(sdi+ssdi);
 
   int N;
-  if (me==0) {N=Nu;} //domaine 1
-  else if (me==1 ) {N=Nv;}//domaine 2
+  if (me==0) N=Nu; //domaine 1
+  else if (me==1 ) N=Nv;//domaine 2
 
   //construction de la matrice sur le domaine
   for(int i=0; i< N*Ny; i++){
 
     //remplissage des termes diagonaux du bloc diagonal
-    if ((i+1)%N==0 && me==0) {//domaine 1 //modification de la dernière ligne
+    if ((i+1)%N==0 && me==0) {//domaine 1 //modification de la derniere ligne
       col.push_back(i);
       row.push_back(i);
       val.push_back(di+D*dt*beta/(dx*alpha));
     }
 
-    else if((i+1)%N==1 && me==1){//domaine 2 //modification de la première ligne
+    else if((i+1)%N==1 && me==1){//domaine 2 //modification de la premiere ligne
       col.push_back(i);
       row.push_back(i);
       val.push_back(di+D*dt*beta/(dx*alpha));
@@ -43,12 +43,12 @@ void Matrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val
       val.push_back(di);
     }
 
-    if((i+1)>=N*Ny){} //test de la dernière ligne/colonne
+    if((i+1)>=N*Ny){} //test de la derniere ligne/colonne
 
     //remplissage des termes de la sur-diagonale et sous-diagonale du bloc diagonal
     else{
 
-      col.push_back(i+1);//sur-diag inchangée quelque soit le domaine
+      col.push_back(i+1);//sur-diag inchangee quelque soit le domaine
       row.push_back(i);
       val.push_back(sdi);
 
@@ -78,17 +78,17 @@ void Matrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val
       val.push_back(ssdi);
       col.push_back(i+Ny); //stockage de la sur-sur-diagonale (symetrie)
       row.push_back(i);
-      val.push_back(ssdi);}
+      val.push_back(ssdi);
     }
   }
 
 
   // ---------------------------------------------------------
-  //fonction qui remplit le vecteur second membre du problème
+  //fonction qui remplit le vecteur second membre du probleme
 
-  void secondMembre_seq(std::vector<double> &S,std::vector<double> U, std::vector<double> V, int Nx, int Ny, double dt,double t, double Lx, double Ly, double D, int mode, double alpha, double beta, int h_part, int me){
+  void secondMembre(std::vector<double> &S,std::vector<double> U, std::vector<double> V, int Nx, int Ny,int N, double dt,double t, double Lx, double Ly, double D, int mode, double alpha, double beta, int me){
 
-    // Fonction qui remplit le second membre selon le domaine où l'on se trouve
+    // Fonction qui remplit le second membre selon le domaine ou l'on se trouve
 
     double dx=Lx/(Nx+1),dy=Ly/(Ny+1); //pas d'espace
     double gamma=D*dt/(dx*dx);
@@ -96,32 +96,32 @@ void Matrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val
 
     for(int j=0; j<Ny; j++){
       if (me==0){ //domaine 1
-        for(int i=0; i<Nu; i++){
+        for(int i=0; i<N; i++){
 
-          S[i+j*Nu]=U[i+j*Nu]+dt*f((i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
+          S[i+j*Nu]=U[i+j*N]+dt*f((i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
 
-          if (j==0) S[i+j*Nu]+=D*dt/dy*dy*g((i+1)*dx,0,mode);
+          if (j==0) S[i+j*N]+=D*dt/dy*dy*g((i+1)*dx,0,mode);
 
-          else if (j==Ny-1) S[i+j*Nu]+=D*dt/(dy*dy)*g((i+1)*dx,Ly,mode);
+          else if (j==Ny-1) S[i+j*N]+=D*dt/(dy*dy)*g((i+1)*dx,Ly,mode);
 
-          else if (i==0) S[i+j*Nu]+=D*dt/dx*dx*h(0,(j+1)*dy,mode);
+          else if (i==0) S[i+j*N]+=D*dt/dx*dx*h(0,(j+1)*dy,mode);
 
-          else if (i==Nu-1) S[i+j*Nu]+=gamma*(V[Ny*2+j]-V[j]) + eta*V[Ny+j];
+          else if (i==N-1) S[i+j*N]+=gamma*(V[Ny*2+j]-V[j]) + eta*V[Ny+j];
         }
       }
       else if (me==1){ //domaine 2
         // Invariant: Nu+Nv-h_part = Nx
-        for(int i=0; i<Nv; i++){
+        for(int i=0; i<N; i++){
 
-          S[i+j*Nu]=U[i+j*Nv]+dt*f((Nu-h_part+i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
+          S[i+j*Nu]=U[i+j*N]+dt*f((Nx-N+i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
 
-          if (j==0) S[i+j*Nv]+=D*dt/dy*dy*g((Nu-h_part+i+1)*dx,0,mode);
+          if (j==0) S[i+j*N]+=D*dt/dy*dy*g((Nx-N+i+1)*dx,0,mode);
 
-          else if (j==Ny-1) S[i+j*Nv]+=D*dt/(dy*dy)*g((Nu-h_part+i+1)*dx,Ly,mode);
+          else if (j==Ny-1) S[i+j*N]+=D*dt/(dy*dy)*g((Nx-N+i+1)*dx,Ly,mode);
 
-          else if (i==0) S[i+j*Nv]+=gamma*(V[j]-V[Ny*2+j]) + eta*V[Ny+j];
+          else if (i==0) S[i+j*N]+=gamma*(V[j]-V[Ny*2+j]) + eta*V[Ny+j];
 
-          else if (i==Nv-1) S[i+j*Nv]+=D*dt/dx*dx*h(Lx,(j+1)*dy,mode);
+          else if (i==N-1) S[i+j*N]+=D*dt/dx*dx*h(Lx,(j+1)*dy,mode);
         }
       }
     }
