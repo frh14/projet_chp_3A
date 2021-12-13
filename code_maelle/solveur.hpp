8e131c877ee41e77af75afe48c52a97cd6eeb257
FR <frh01499@gmail.com>
2021-12-13 14:43:44 +0100

    add maelle

--- code_maelle/solveur.hpp
+++ code_maelle/solveur.hpp
@@ -2,8 +2,7 @@
 #define _SOLVEUR_HPP_
 #include <vector>
 
-void CG(std::vector<int> row, std::vector<int> col, std::vector<double> val, std::vector<double> &U, std::vector<double> F,double e,int kmax,int Nx,int Ny);
-void BICG(std::vector<int> row,std::vector<int> col,std::vector<double> val,std::vector<double> &X,std::vector<double> F, double e, int kmax,int Nx,int Ny);
+void BICG(std::vector<int> row,std::vector<int> col,std::vector<double> val,std::vector<double> &X,std::vector<double> &F, double e, int kmax,int Nx,int Ny);
 double norm2(std::vector<double> x);
 double ps(std::vector<double> x,std::vector<double> y);
 void mulSparseMatrix(std::vector<int> row, std::vector<int> col, std::vector<double> val, std::vector<double> &y, std::vector<double> x);
