d5e953278f3d5e69240f66a3ed1f42db75a51c85
alixkien <alix.kien@gmail.com>
2022-01-26 23:16:18 +0100

    Implantation de la //

--- matrix.cpp
+++ matrix.cpp
@@ -24,7 +24,7 @@ void Matrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val
   //printf("sdi=%f , ssdi=%f , di=%f , gamma=%f \n",sdi,ssdi,di,gamma);
 
   for(int i=0; i<(N*Ny); i++){
-    
+
     if (me==0) {
       if((i+1)%N==0){}
       else{
@@ -43,38 +43,38 @@ void Matrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val
           val.push_back(sdi);
         }
 
-	if (alpha==0){ // Suppression du terme ajoute si:
-	  if(me==0 && (i+1)%N==(N-1)){ // domaine 1 derniere ligne
-	    col.pop_back();
-	    row.pop_back();
-	    val.pop_back();
-	  }
-	  else if(me==1 && (i+1)%N==0){ // domaine 2 premiere ligne
-	    col.pop_back();
-	    row.pop_back();
-	    val.pop_back();
-	  }
-	  else{}
-	}
+        if (alpha==0){ // Suppression du terme ajoute si:
+          if(me==0 && (i+1)%N==(N-1)){ // domaine 1 derniere ligne
+            col.pop_back();
+            row.pop_back();
+            val.pop_back();
+          }
+          else if(me==1 && (i+1)%N==0){ // domaine 2 premiere ligne
+            col.pop_back();
+            row.pop_back();
+            val.pop_back();
+          }
+          else{}
+        }
 
         col.push_back(i+1); //stockage de la sur-diagonale (symetrie)
         row.push_back(i);
         val.push_back(sdi);
 
-	if (alpha==0){ // Suppression du terme ajoute si:
-	  if(me==0 && i%N==(N-1)){ // domaine 1 derniere ligne
-	    col.pop_back();
-	    row.pop_back();
-	    val.pop_back();
-	  }
-	  else if(me==1 && i%N==0){ // domaine 2 premiere ligne
-	    col.pop_back();
-	    row.pop_back();
-	    val.pop_back();
-	  }
-	  else{}
-	}
-	
+        if (alpha==0){ // Suppression du terme ajoute si:
+          if(me==0 && i%N==(N-1)){ // domaine 1 derniere ligne
+            col.pop_back();
+            row.pop_back();
+            val.pop_back();
+          }
+          else if(me==1 && i%N==0){ // domaine 2 premiere ligne
+            col.pop_back();
+            row.pop_back();
+            val.pop_back();
+          }
+          else{}
+        }
+
       }
     }
 
@@ -93,38 +93,38 @@ void Matrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val
           val.push_back(sdi);
         }
 
-	if (alpha==0){ // Supression du terme ajoute si:
-	  if(me==0 && i%N==(N-1)){ // domaine 1 derniere ligne
-	    col.pop_back();
-	    row.pop_back();
-	    val.pop_back();
-	  }
-	  else if(me==1 && i%N==0){ // domaine 2 premiere ligne
-	    col.pop_back();
-	    row.pop_back();
-	    val.pop_back();
-	  }
-	  else{}
-	}
+        if (alpha==0){ // Supression du terme ajoute si:
+          if(me==0 && i%N==(N-1)){ // domaine 1 derniere ligne
+            col.pop_back();
+            row.pop_back();
+            val.pop_back();
+          }
+          else if(me==1 && i%N==0){ // domaine 2 premiere ligne
+            col.pop_back();
+            row.pop_back();
+            val.pop_back();
+          }
+          else{}
+        }
 
         col.push_back(i); //stockage de la sous-diagonale
         row.push_back(i+1);
         val.push_back(sdi);
 
-	if (alpha==0){ // Supression du terme ajoute si:
-	  if(me==0 && (i+1)%N==(N-1)){ // domaine 1 derniere ligne
-	    col.pop_back();
-	    row.pop_back();
-	    val.pop_back();
-	  }
-	  else if(me==1 && (i+1)%N==0){ // domaine 2 premiere ligne
-	    col.pop_back();
-	    row.pop_back();
-	    val.pop_back();
-	  }
-	  else{}
-	}
-	
+        if (alpha==0){ // Supression du terme ajoute si:
+          if(me==0 && (i+1)%N==(N-1)){ // domaine 1 derniere ligne
+            col.pop_back();
+            row.pop_back();
+            val.pop_back();
+          }
+          else if(me==1 && (i+1)%N==0){ // domaine 2 premiere ligne
+            col.pop_back();
+            row.pop_back();
+            val.pop_back();
+          }
+          else{}
+        }
+
       }
     }
 
@@ -134,96 +134,280 @@ void Matrix(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val
       val.push_back(ssdi);
 
       if (alpha==0){ // Suppression du terme ajoute si:
-	if(me==0 && (i+N)%N==(N-1)){ // domaine 1 derniere ligne
-	  col.pop_back();
-	  row.pop_back();
-	  val.pop_back();
-	}
-	else if(me==1 && (i+N)%N==0){ // domaine2 premiere ligne
-	  col.pop_back();
-	  row.pop_back();
-	  val.pop_back();
-	}
-	else{}
+        if(me==0 && (i+N)%N==(N-1)){ // domaine 1 derniere ligne
+          col.pop_back();
+          row.pop_back();
+          val.pop_back();
+        }
+        else if(me==1 && (i+N)%N==0){ // domaine2 premiere ligne
+          col.pop_back();
+          row.pop_back();
+          val.pop_back();
+        }
+        else{}
       }
-      
+
       col.push_back(i+N); //stockage de la sur-sur-diagonale (symetrie)
       row.push_back(i);
       val.push_back(ssdi);
 
       if (alpha==0){ // Suppression du terme ajoute si:
-	if(me==0 && i%N==(N-1)){ // domaine 1 derniere ligne
-	  col.pop_back();
-	  row.pop_back();
-	  val.pop_back();
-	}
-	else if(me==1 && i%N==0){ // domaine 2 premiere ligne
-	  col.pop_back();
-	  row.pop_back();
-	  val.pop_back();
-	}
-	else{}
+        if(me==0 && i%N==(N-1)){ // domaine 1 derniere ligne
+          col.pop_back();
+          row.pop_back();
+          val.pop_back();
+        }
+        else if(me==1 && i%N==0){ // domaine 2 premiere ligne
+          col.pop_back();
+          row.pop_back();
+          val.pop_back();
+        }
+        else{}
       }
     }
-    
+
     if ((i+1)%N==0 && me==0) { //domaine 1 : modification de la derniere ligne
-      col.push_back(i),row.push_back(i),val.push_back(gamma);}
+      col.push_back(i),row.push_back(i),val.push_back(gamma);
+    }
 
     else if((i+1)%N==1 && me==1){ //domaine 2 : modification de la premiere ligne
-      col.push_back(i),row.push_back(i),val.push_back(gamma);}
+      col.push_back(i),row.push_back(i),val.push_back(gamma);
+    }
 
     else{col.push_back(i),row.push_back(i),val.push_back(di);}
-    
+
   }
-  
+
 }
 
 // -----------------------------------------------------------------------------
 //fonction qui remplit le vecteur second membre du probleme selon le domaine ou l'on se trouve
 
-  void secondMembre(std::vector<double> &S,std::vector<double> U, std::vector<double> V, int Nx, int Ny,int N, double dt,double t, double Lx, double Ly, double D, int mode, double alpha, double beta, int me){
+void secondMembre(std::vector<double> &S,std::vector<double> U, std::vector<double> V, int Nx, int Ny,int N, double dt,double t, double Lx, double Ly, double D, int mode, double alpha, double beta, int me){
 
-    double dx=Lx/(Nx+1),dy=Ly/(Ny+1); //pas d'espace
-    double delta=D*dt/(dx*dx),eta(1);
+  double dx=Lx/(Nx+1),dy=Ly/(Ny+1); //pas d'espace
+  double delta=D*dt/(dx*dx),eta(1);
 
-    if (alpha != 0) eta=2*D*dt*beta/(dx*alpha);
-    else eta=delta;
+  if (alpha != 0) eta=2*D*dt*beta/(dx*alpha);
+  else eta=delta;
 
-    for(int j=0; j<Ny; j++){
-      for(int i=0; i<N; i++){
+  for(int j=0; j<Ny; j++){
+    for(int i=0; i<N; i++){
 
-	S[i+j*N]=U[i+j*N];
+      S[i+j*N]=U[i+j*N];
 
-        if (me==0){ //domaine 1
+      if (me==0){ //domaine 1
 
-          S[i+j*N]+=dt*f((i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
+        S[i+j*N]+=dt*f((i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
 
-          if (j==0) S[i+j*N]+=D*dt*g((i+1)*dx,0,mode)/(dy*dy);
+        if (j==0) S[i+j*N]+=D*dt*g((i+1)*dx,0,mode)/(dy*dy);
 
-          else if (j==Ny-1) S[i+j*N]+=D*dt*g((i+1)*dx,Ly,mode)/(dy*dy);
+        if (j==Ny-1) S[i+j*N]+=D*dt*g((i+1)*dx,Ly,mode)/(dy*dy);
 
-          if (i==0) S[i+j*N]+=D*dt*h(0,(j+1)*dy,mode)/(dx*dx);
+        if (i==0) S[i+j*N]+=D*dt*h(0,(j+1)*dy,mode)/(dx*dx);
 
-          else if (i==N-1){
-	    if (alpha==0) S[i+j*N]=eta*V[Ny+j];
-	    else S[i+j*N]+=delta*(V[2*Ny+j]-V[j])+eta*V[Ny+j];
-	  }
+        if (i==N-1){
+          if (alpha==0) S[i+j*N]=eta*V[Ny+j];
+          else S[i+j*N]+=delta*(V[2*Ny+j]-V[j])+eta*V[Ny+j];
+        }
+      }
+
+      else if (me==1){ //domaine 2
+
+        S[i+j*N]+=dt*f((Nx-N+i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
+
+        if (j==0) S[i+j*N]+=D*dt*g((Nx-N+i+1)*dx,0,mode)/(dy*dy);
+
+        if (j==Ny-1) S[i+j*N]+=D*dt*g((Nx-N+i+1)*dx,Ly,mode)/(dy*dy);
+
+        if (i==0){
+          if (alpha==0) S[i+j*N]=eta*V[Ny+j];
+          else S[i+j*N]+=delta*(V[j]-V[2*Ny+j])+eta*V[Ny+j];
         }
 
-        else if (me==1){ //domaine 2
+        if (i==N-1) S[i+j*N]+=D*dt*h(Lx,(j+1)*dy,mode)/(dx*dx);
+      }
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+//fonction qui remplit la matrice de diffusion sur un proc
+
+void Matrix_(std::vector<int> &row,std::vector<int> &col,std::vector<double> &val,int Nx, int Ny, int N, int Nproc, double Lx, double Ly, double D, double dt, double alpha, double beta, int me){
+
+  double dx=Lx/(Nx+1),dy=Ly/(Ny+1); //pas d'espace
+
+  double di(0),sdi(0),ssdi(0), gamma(0);
+  sdi=-dt*D/(dx*dx);
+  ssdi=-dt*D/(dy*dy);
+  di=1-2*(sdi+ssdi);
+
+  gamma = di+2*D*dt*beta/(dx*alpha);
+
+  printf("sdi=%f , ssdi=%f , di=%f , di+2*D*dt*beta/(dx*alpha)=%f \n",sdi,ssdi,di,di+2*D*dt*beta/(dx*alpha));
+
+  for(int i=0; i<(N*Ny); i++){
+
+    if (me==0) { // Proc 0
+
+      if ((i+1)%N==0) {//domaine 1 //modification de la derniere ligne
+        col.push_back(i),row.push_back(i),val.push_back(gamma);
+      }
+
+      else{col.push_back(i),row.push_back(i),val.push_back(di);}
+
+      if((i+1)%N==0){}
+      else{
+        if ((i+1)%N==N-1) {
+          col.push_back(i); //stockage de la sous-diagonale
+          row.push_back(i+1);
+          val.push_back(2*sdi);
+        }
+        else if((i+1)%N==N){}
+        else{
+          col.push_back(i); //stockage de la sous-diagonale
+          row.push_back(i+1);
+          val.push_back(sdi);
+        }
+
+        col.push_back(i+1); //stockage de la sur-diagonale (symetrie)
+        row.push_back(i);
+        val.push_back(sdi);
+      }
+    }
+
+    else if (me==Nproc-1) { // Proc Nproc-1
+
+      if((i+1)%N==1){//domaine 2 //modification de la premiere ligne
+        col.push_back(i),row.push_back(i),val.push_back(gamma);
+      }
+
+      else{col.push_back(i),row.push_back(i),val.push_back(di);}
+
+      if((i+1)%N==0){}
+      else{
+        if (i%N==0) {//stockage de la sur-diag
+          col.push_back(i+1); //stockage de la sur-diagonale (symetrie)
+          row.push_back(i);
+          val.push_back(2*sdi);
+        }
+
+        else{
+          col.push_back(i+1); //stockage de la sur-diagonale (symetrie)
+          row.push_back(i);
+          val.push_back(sdi);
+        }
+
+        col.push_back(i); //stockage de la sous-diagonale
+        row.push_back(i+1);
+        val.push_back(sdi);
+      }
+    }
+
+    else{
+      if ((i+1)%N==0) {//domaine 1 //modification de la derniere ligne
+        col.push_back(i),row.push_back(i),val.push_back(gamma);
+      }
+
+      else if((i+1)%N==1){//domaine 2 //modification de la premiere ligne
+        col.push_back(i),row.push_back(i),val.push_back(gamma);
+      }
+
+      else{col.push_back(i),row.push_back(i),val.push_back(di);}
+
+      if((i+1)%N==0){}
+      else{
+        if (i%N==0) {//stockage de la sur-diag
+          col.push_back(i+1); //sur-diagonale
+          row.push_back(i);
+          val.push_back(2*sdi);
+        }
+
+        else{
+          col.push_back(i+1); //sur-diagonale
+          row.push_back(i);
+          val.push_back(sdi);
+        }
+
+        if ((i+1)%N==N-1) {
+          col.push_back(i); //sous-diagonale
+          row.push_back(i+1);
+          val.push_back(2*sdi);
+        }
+
+        else if((i+1)%N==N){}
+
+        else{
+          col.push_back(i); //sous-diagonale
+          row.push_back(i+1);
+          val.push_back(sdi);
+        }
+      }
+    }
+
+    if(i+N<N*Ny){
+      col.push_back(i); //stockage de la sous-sous-diagonale
+      row.push_back(i+N);
+      val.push_back(ssdi);
+      col.push_back(i+N); //stockage de la sur-sur-diagonale (symetrie)
+      row.push_back(i);
+      val.push_back(ssdi);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+//fonction qui remplit le vecteur second membre selon le proc
+
+void secondMembre_(std::vector<double> &S,std::vector<double> U, std::vector<double> V1,std::vector<double> V2, int Nx, int Ny, int N, int Nproc,double dt,double t, double Lx, double Ly, double D, int mode, double alpha, double beta, int me){
+
+  double dx=Lx/(Nx+1),dy=Ly/(Ny+1); //pas d'espace
+  double delta=D*dt/(dx*dx);
+  double eta=2*D*dt*beta/(dx*alpha);
+
+  for(int j=0; j<Ny; j++){
+    for(int i=0; i<N; i++){
+
+      S[i+j*N]=U[i+j*N];
+
+      if (me==0){ // Proc 0
+
+        S[i+j*N]+=dt*f((i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
+
+        if (j==0) S[i+j*N]+=D*dt*g((i+1)*dx,0,mode)/(dy*dy);
+
+        if (j==Ny-1) S[i+j*N]+=D*dt*g((i+1)*dx,Ly,mode)/(dy*dy);
+
+        if (i==0) S[i+j*N]+=D*dt*h(0,(j+1)*dy,mode)/(dx*dx);
+
+        if (i==N-1)S[i+j*N]+=delta*(V1[2*Ny+j]-V1[j])+eta*V1[Ny+j];
+      }
+
+      else if (me==Nproc-1){ // Proc Nproc-1
+
+        S[i+j*N]+=dt*f((Nx-N+i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
+
+        if (j==0) S[i+j*N]+=D*dt*g((Nx-N+i+1)*dx,0,mode)/(dy*dy);
+
+        if (j==Ny-1) S[i+j*N]+=D*dt*g((Nx-N+i+1)*dx,Ly,mode)/(dy*dy);
+
+        if (i==0) S[i+j*N]+=delta*(V2[j]-V2[2*Ny+j])+eta*V2[Ny+j];
+
+        if (i==N-1) S[i+j*N]+=D*dt*h(Lx,(j+1)*dy,mode)/(dx*dx);}
+
+        else{
 
           S[i+j*N]+=dt*f((Nx-N+i+1)*dx,(j+1)*dy,t,Lx,Ly,mode);
 
           if (j==0) S[i+j*N]+=D*dt*g((Nx-N+i+1)*dx,0,mode)/(dy*dy);
 
-          else if (j==Ny-1) S[i+j*N]+=D*dt*g((Nx-N+i+1)*dx,Ly,mode)/(dy*dy);
+          if (j==Ny-1) S[i+j*N]+=D*dt*g((Nx-N+i+1)*dx,Ly,mode)/(dy*dy);
 
-          if (i==0){
-	    if (alpha==0) S[i+j*N]=eta*V[Ny+j];
-	    else S[i+j*N]+=delta*(V[j]-V[2*Ny+j])+eta*V[Ny+j];
-	  }
+          if (i==0) S[i+j*N]+=delta*(V2[j]-V2[2*Ny+j])+eta*V2[Ny+j];
 
-          else if (i==N-1) S[i+j*N]+=D*dt*h(Lx,(j+1)*dy,mode)/(dx*dx);}
+          if (i==N-1) S[i+j*N]+=delta*(V1[2*Ny+j]-V1[j])+eta*V1[Ny+j];
+        }
       }
     }
   }
--- update.cpp
+++ update.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <mpi.h>
 
 #include "fonctions.hpp"
 #include "matrix.hpp"
@@ -26,7 +27,7 @@ void Update(std::vector<double> U,std::vector<double> V,int Nx,int Ny,int Nu,int
 
   double error(1.); int iteschwz(0);
   double t;
-  
+
   //construction matrices de resolution
   //domaine 1
   Matrix(rowu,colu,valu,Nx,Ny,Nu,Lx,Ly,D,dt,alpha,beta,0);
@@ -43,9 +44,11 @@ void Update(std::vector<double> U,std::vector<double> V,int Nx,int Ny,int Nu,int
   }
   printf("\n");
   */
-  
+
   //schema en temps
   for (int k = 0; k < Nt; k++){
+
+    printf("\n Begin time iteration %d\n",k);
 
     t = k*dt; //temps de l'experience
 
@@ -54,7 +57,7 @@ void Update(std::vector<double> U,std::vector<double> V,int Nx,int Ny,int Nu,int
 
     //Boucle de Schwarz
     while ((iteschwz <= maxschwz) && (error >= errschwz)){
-      
+
       //construction du second membre sur chacun des sous-domaines
       std::vector<double> Su(Nu*Ny,0.);
       std::vector<double> Sv(Nv*Ny,0.);
@@ -74,22 +77,24 @@ void Update(std::vector<double> U,std::vector<double> V,int Nx,int Ny,int Nu,int
 	U0[j+Ny] = V[Nv*j+h_part-1];
 	U0[j+2*Ny] = V[Nv*j+h_part];
       }
-      
+
       // Evaluation de l'erreur pour la condition d'arret error
       error = maj_error(U,V,h_part,Ny,Nu,Nv);
       //printf("ite %d   erreur de schwarz=%f \n",iteschwz,error);
       iteschwz++;
     }
-    if (iteschwz>=maxschwz) printf("Schwarz n'a pas convergé ! Erreur Schwarz : %f\n",error);
+    if (iteschwz>=maxschwz) printf("\n Schwarz n'a pas convergé ! Erreur Schwarz : %f\n",error);
+
+    printf("\n End time iteration %d , time = %f\n",k,t);
 
     //ecriture dans les  fichiers
     Write(U,V,Nx,Ny,Nu,Nv,Lx,Ly,h_part,k);
-    
+
   }
 }
 
 void Write(std::vector<double> U,std::vector<double> V,int Nx,int Ny,int Nu,int Nv,double Lx,double Ly,int h_part,int k){
-  
+
   std::string prefixeu="solutionU";
   std::ostringstream oss;
   oss<<prefixeu<<k<<".txt";
@@ -113,4 +118,173 @@ void Write(std::vector<double> U,std::vector<double> V,int Nx,int Ny,int Nu,int 
   for (int i = 0; i < Nv; i++){
     for (int j = 0; j < Ny; j++) NewFichierv<< x_tab[i+Nu-h_part] <<" "<<  y_tab[j] << " " << V[i+j*Nv] << std::endl;
     }
+}
+
+void Update_(int argc, char** argv,int Nx,int Ny,double dt,double Lx,double Ly,double D,int mode,int h_part,double alpha,double beta,int Nt,double e,int kmax,double errschwz,int maxschwz){
+
+  //construction des stencils
+  std::vector<double> U0(3*Ny); //domaine 1 pour le domaine 2
+  std::vector<double> V0(3*Ny); //domaine 2 pour le domaine 1
+
+  //schema en temps
+  for (int k = 0; k < Nt; k++){
+
+    double t = k*dt; //temps de l'experience
+
+    //Initialisation des stencils
+    for (int i = 0; i < U0.size(); i++) U0[i]=1.,V0[i]=1.;
+
+    MPI_Init(&argc,&argv);
+    int me,Nproc;
+    MPI_Comm_rank(MPI_COMM_WORLD,&me);  //processeur local
+    MPI_Comm_size(MPI_COMM_WORLD,&Nproc); //nombre de processeur total utilisés
+
+    printf("thread %d on %d \n",me,Nproc);
+
+    int IBeg, IEnd;
+
+    charge(Nx,Nproc,me,IBeg,IEnd);
+
+    if (me==0) {}
+    else{IBeg-=h_part;}
+
+    int N=IEnd-IBeg+1;
+
+    printf("thread %d N==%d \n",me,N);
+
+    std::vector<double> U_loc(N*Ny,1);
+
+    std::vector<double> U0_loc(3*Ny,0.);
+    std::vector<double> V0_loc(3*Ny,0.);
+
+    std::vector<int> row_loc, col_loc;
+    std::vector<double> val_loc;
+
+    Matrix_(row_loc,col_loc,val_loc,Nx,Ny,N,Nproc,Lx,Ly,D,dt,alpha,beta,me);
+
+    MPI_Status Status;
+
+    int iteschwz_loc=0.;
+    double error_loc=1.;
+
+    while ((iteschwz_loc <= maxschwz) && (error_loc >= errschwz)){
+
+      std::vector<double> S_loc(N*Ny,0.);
+
+      secondMembre_(S_loc,U_loc,U0_loc,V0_loc,Nx,Ny,N,Nproc,dt,t,Lx,Ly,D,mode,alpha,beta,me);
+
+      BICGStab(row_loc,col_loc,val_loc,U_loc,S_loc,e,kmax,N,Ny);
+
+      if (me==0) { // Proc 0
+        for (int j = 0; j < Ny; j++){
+          // Proc 0 envoit U a proc 1
+          MPI_Send(&U_loc[N*j+N-h_part-1],1,MPI_DOUBLE,me+1,0,MPI_COMM_WORLD);
+          MPI_Send(&U_loc[N*j+N-h_part],1,MPI_DOUBLE,me+1,1,MPI_COMM_WORLD);
+          MPI_Send(&U_loc[N*j+N-h_part+1],1,MPI_DOUBLE,me+1,2,MPI_COMM_WORLD);
+
+          // Proc 0 recoit stencil droite U0 de proc 1
+          MPI_Recv(&U0_loc[j],1,MPI_DOUBLE,me+1,0,MPI_COMM_WORLD,&Status);
+          MPI_Recv(&U0_loc[j+Ny],1,MPI_DOUBLE,me+1,1,MPI_COMM_WORLD,&Status);
+          MPI_Recv(&U0_loc[j+2*Ny],1,MPI_DOUBLE,me+1,2,MPI_COMM_WORLD,&Status);
+        }
+      }
+
+      else if (me==Nproc-1) { // Proc Nproc-1
+        for (int j = 0; j < Ny; j++){
+          // Proc Nproc-1 envoit U a proc Nproc-2
+          MPI_Send(&U_loc[N*j+h_part-2],1,MPI_DOUBLE,me-1,0,MPI_COMM_WORLD);
+          MPI_Send(&U_loc[N*j+h_part-1],1,MPI_DOUBLE,me-1,1,MPI_COMM_WORLD);
+          MPI_Send(&U_loc[N*j+h_part],1,MPI_DOUBLE,me-1,2,MPI_COMM_WORLD);
+
+          // Proc Nproc-1 recoit stencil gauche V0 de proc Nproc-2
+          MPI_Recv(&V0_loc[j],1,MPI_DOUBLE,me-1,0,MPI_COMM_WORLD,&Status);
+          MPI_Recv(&V0_loc[j+Ny],1,MPI_DOUBLE,me-1,1,MPI_COMM_WORLD,&Status);
+          MPI_Recv(&V0_loc[j+2*Ny],1,MPI_DOUBLE,me-1,2,MPI_COMM_WORLD,&Status);
+        }
+      }
+
+      else {
+        for (int j = 0; j < Ny; j++){
+          // Proc me envoit U a proc me-1
+          MPI_Send(&U_loc[N*j+h_part-2],1,MPI_DOUBLE,me-1,0,MPI_COMM_WORLD);
+          MPI_Send(&U_loc[N*j+h_part-1],1,MPI_DOUBLE,me-1,1,MPI_COMM_WORLD);
+          MPI_Send(&U_loc[N*j+h_part],1,MPI_DOUBLE,me-1,2,MPI_COMM_WORLD);
+
+          // Proc me recoit stencil gauche V0 de proc me-1
+          MPI_Recv(&V0_loc[j],1,MPI_DOUBLE,me-1,0,MPI_COMM_WORLD,&Status);
+          MPI_Recv(&V0_loc[j+Ny],1,MPI_DOUBLE,me-1,1,MPI_COMM_WORLD,&Status);
+          MPI_Recv(&V0_loc[j+2*Ny],1,MPI_DOUBLE,me-1,2,MPI_COMM_WORLD,&Status);
+
+          // Proc me envoit U a proc me+1
+          MPI_Send(&U_loc[N*j+N-h_part-1],1,MPI_DOUBLE,me+1,0,MPI_COMM_WORLD);
+          MPI_Send(&U_loc[N*j+N-h_part],1,MPI_DOUBLE,me+1,1,MPI_COMM_WORLD);
+          MPI_Send(&U_loc[N*j+N-h_part+1],1,MPI_DOUBLE,me+1,2,MPI_COMM_WORLD);
+
+          // Proc me recoit stencil droite U0 de proc me+1
+          MPI_Recv(&U0_loc[j],1,MPI_DOUBLE,me+1,0,MPI_COMM_WORLD,&Status);
+          MPI_Recv(&U0_loc[j+Ny],1,MPI_DOUBLE,me+1,1,MPI_COMM_WORLD,&Status);
+          MPI_Recv(&U0_loc[j+2*Ny],1,MPI_DOUBLE,me+1,2,MPI_COMM_WORLD,&Status);
+        }
+      }
+
+      std::vector<double> V_loc(h_part*Ny,0.);
+
+      if (me!=0) {
+        for (int j = 0; j < Ny; j++) {
+          for (int i = 0; i < h_part; i++) {
+            MPI_Send(&U_loc[j*N+i],1,MPI_DOUBLE,me-1,10,MPI_COMM_WORLD);
+          }
+        }
+      }
+
+      if (me!=Nproc-1) {
+        double error_max(0.);
+        for (int j=0; j<Ny; j++){
+          for (int i=0; i<h_part; i++ ){
+            MPI_Recv(&V_loc[i+j*h_part],1,MPI_DOUBLE,me+1,10,MPI_COMM_WORLD,&Status);
+          }
+        }
+
+        for (int j=0; j<Ny; j++){
+          for (int i=0; i<h_part; i++ ){
+            error_loc = abs(U_loc[j*N+N-h_part+i]-V_loc[i+j*h_part]);
+            if (error_loc>error_max) error_max=error_loc;
+          }
+        }
+        error_loc=error_max;
+
+        MPI_Send(&error_loc,1,MPI_DOUBLE,me+1,10,MPI_COMM_WORLD);
+      }
+
+      if (me!=0) {
+        MPI_Recv(&error_loc,1,MPI_DOUBLE,me-1,10,MPI_COMM_WORLD,&Status);
+      }
+
+      iteschwz_loc++;
+    }
+
+    Write_(U_loc,IBeg,IEnd,Nx,Ny,N,Lx,Ly,h_part,k,me);
+
+  }
+  MPI_Finalize();
+
+}
+
+void Write_(std::vector<double> U_loc,int IBeg,int IEnd,int Nx,int Ny,int N,double Lx,double Ly,int h_part,int k,int me){
+
+  std::string prefix="solution";
+  std::ostringstream oss;
+  oss<<prefix<<me<<".txt";
+  std::ofstream NewFichier(oss.str().c_str(), std::ios::out | std::ios::trunc);
+
+  std::vector<double> x_tab_loc,y_tab_loc;
+  for (int i = IBeg; i < IEnd; i++) x_tab_loc.push_back(i*Lx/(Nx+1));
+  for (int j = 0; j < Ny; j++) y_tab_loc.push_back(j*Ly/(Ny+1));
+
+  for (int i = 0; i < N-1; i++){
+    for (int j = 0; j < Ny; j++){
+      NewFichier<< x_tab_loc[i] <<" "<<  y_tab_loc[j] << " " << U_loc[i+j*N] << std::endl;
+    }
+  }
+
 }
