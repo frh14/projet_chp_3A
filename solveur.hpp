9e3b73cc2b2c08a86bff9df1ddcd7fbc637f3cea
alixkien <alix.kien@gmail.com>
2022-01-28 10:22:43 +0100

    clean solveur + maj parameters.dat

--- solveur.hpp
+++ solveur.hpp
@@ -6,6 +6,5 @@ double norm2(std::vector<double> x);
 double ps(std::vector<double> x,std::vector<double> y);
 void mulSparseMatrix(std::vector<int> row, std::vector<int> col, std::vector<double> val, std::vector<double> &y, std::vector<double> x);
 void transpSparseMatrix(std::vector<int> row, std::vector<int> col, std::vector<double> val,std::vector<int> &rowT, std::vector<int> &colT, std::vector<double> &valT );
-std::vector<double> BICGStabTest(std::vector<int> row,std::vector<int> col,std::vector<double> val,std::vector<double> &F,double e,int kmax,int Nx,int Ny);
 
 #endif
